6354a9c27c9059bdccf8a82e7aa37fa07b72c401
blackbrokkoli <kastomat@gmail.com>
2020-12-04 19:19:18 +0100

    loads the first two images form directory and shows them next to each other

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 extern crate docopt;
 extern crate rustc_serialize;
 extern crate image;
@@ -12,11 +14,26 @@ fn main() {
     // get path
     let path = args.arg_path;
 
-    let image_option = image::open(&path);
+    // THIS IS HORRIBLE
+    let mut first_image_path = String::from("");
+    let mut second_image_path = String::from("");
+
+    let mut paths = fs::read_dir(&path).unwrap().take(1);
+    for p in paths {
+        first_image_path = p.unwrap().path().display().to_string();
+    }
+
+    paths = fs::read_dir(&path).unwrap().take(2);
+    for p in paths {
+        second_image_path = p.unwrap().path().display().to_string();
+    }
+
+    let image_left = image::open(&first_image_path);
+    let image_right = image::open(&second_image_path);
     
-    match image_option {
+    match &image_left {
         Ok(image) => {
-            let error = window::display_image(image);
+            let error = window::display_image(image_left.unwrap(), image_right.unwrap());
             if let Err(err) = error {
                 println!("Error while opening window: {}", err);
             }
--- src/window.rs
+++ src/window.rs
@@ -5,9 +5,9 @@ use image_to_window::image_to_framebuffer;
 use std::cmp;
 
 
+pub fn display_image(image_left: DynamicImage, image_right: DynamicImage) -> Result<(), Error> {
 
-pub fn display_image(image: DynamicImage) -> Result<(), Error> {
-
+    // General options for the window itself
     let window_options = WindowOptions {
         borderless: false,
         title: true,
@@ -15,15 +15,15 @@ pub fn display_image(image: DynamicImage) -> Result<(), Error> {
         scale: Scale::FitScreen
     };
     
-    // define images
-    // type RgbaImage = ImageBuffer<Rgba<u8>, Vec<u8>>;
-    // Sendable Rgb + alpha channel image buffer
-    let rgba_image_left = image.to_rgba();
-    let rgba_image_right = image.to_rgba();
+    // define the two images
+    let rgba_image_left = image_left.to_rgba();
+    let rgba_image_right = image_right.to_rgba();
 
+    // set the window dimensions, calculated via the image dimensions
     let window_width = rgba_image_left.dimensions().0 + rgba_image_right.dimensions().0;
     let window_height = cmp::max(rgba_image_left.dimensions().1, rgba_image_right.dimensions().1);
 
+    // create the window
     let mut window = Window::new(
         "Image Viewer", 
         window_width as usize, 
@@ -31,57 +31,35 @@ pub fn display_image(image: DynamicImage) -> Result<(), Error> {
         window_options
     )?;
 
-    // let frame_buffer = image_to_framebuffer(image);
+    // create an image buffer
     let mut buffer: Vec<u32> = vec![0; window_width as usize * window_height as usize];
-    
-    // for i in buffer.iter_mut() {
-    //     //println!("uwu what dis {}", i);
-    // }
-    
-    // Loop rows
 
-    // Image A is 3 wide
-    // Image B is 5 wide
-    // 0-2 should be A
-    // 3-7 should be B
-
-    // In Column 4 we want to get B pixel 2
-    // Getting B pixel in general:
-    // Column + 1 - A.width 
-    // 4 + 1 - 3 = 2
-
-
+    // paint the images (or rather, the whole UI)
+    // we are not looping the buffer, but rather each row and each column
     for row in 0..window_height {
         for column in 0..window_width {
+            // calculate the index in the onedimensional buffer given the two dimensional coords
             let index = row * window_width + column;
-            if column < rgba_image_left.dimensions().0 {
-                let relevant_image_pixel = rgba_image_left.get_pixel(column, row);
-                let rgba = relevant_image_pixel.to_rgba();
-                let buffer_pixel = pixel_to_buffer_pixel(rgba[0], rgba[1], rgba[2], rgba[3]);
-                buffer[index as usize] = buffer_pixel;
-                // buffer[index as usize] = pixel_to_buffer_pixel(0, 125, 45, 57); 
-            } else {
-                let image_pixel_column = column- rgba_image_right.dimensions().0;
-                let relevant_image_pixel = rgba_image_right.get_pixel(image_pixel_column, row);
-                let rgba = relevant_image_pixel.to_rgba();
-                let buffer_pixel = pixel_to_buffer_pixel(rgba[0], rgba[1], rgba[2], rgba[3]);
-                buffer[index as usize] = buffer_pixel;
-                // buffer[index as usize] = pixel_to_buffer_pixel(0, 52, 107, 235); 
+            // set which column from the image we want to paint in this given buffer pixel
+            // the default (=image starting in the top left corner w/o margin) is just the column counter itself
+            let mut image_pixel_column = column;
+            let mut image_to_use = &rgba_image_left;
+            
+            // for the right half
+            if column >= rgba_image_left.dimensions().0 {
+                image_to_use = &rgba_image_right;
+                image_pixel_column = column - image_to_use.dimensions().0;
             }
+
+            let current_pixel = image_to_use.get_pixel(image_pixel_column, row);
+            let rgba = current_pixel.to_rgba();
+            buffer[index as usize] = pixel_to_buffer_pixel(rgba[0], rgba[1], rgba[2], rgba[3]);  
         }
     }
-        // Loop cols
-            // buffer(cols * rowlength)(cols)
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window.update_with_buffer(&buffer);
     }
-
-
-    
-    // while window.is_open() && !window.is_key_down(Key::Escape) {
-    //     window.update_with_buffer(&frame_buffer);
-    // }
 
     Ok(())
 }
@@ -94,23 +72,3 @@ pub fn pixel_to_buffer_pixel(red: u8, green: u8, blue: u8, alpha: u8) -> u32 {
     argb |= blue as u32;
     argb
 }
- 
-// pub fn image_to_framebuffer(image: DynamicImage) -> Vec<u32> {
-//     let rgba_image = image.to_rgba();
-//     let framebuffer: Vec<u32> = rgba_image.pixels().map(|rgba| rgba.channels4())
-//         .map(|(r, g, b, a)| {
-//             let red: u8 = r.into();
-//             let green: u8 = g.into();
-//             let blue: u8 = b.into();
-//             let alpha: u8 = a.into();
-//             let mut argb: u32 = 0;
-//             argb |= (alpha as u32) << 24;
-//             argb |= (red as u32) << 16;
-//             argb |= (green as u32) << 8;
-//             argb |= blue as u32;
-
-//             argb
-//         }).collect();
-    
-//     framebuffer
-// }
